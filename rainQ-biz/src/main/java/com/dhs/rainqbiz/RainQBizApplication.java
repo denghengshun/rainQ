849c57bb5924963ac50326d760823d5626b15af1
dhs <1746343887@qq.com>
2022-11-12 17:17:59 +0800

    init

---
+++ rainQ-biz/src/main/java/com/dhs/rainqbiz/RainQBizApplication.java
@@ -0,0 +1,21 @@
+package com.dhs.rainqbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+/**
+ * 雨qbiz应用程序
+ *
+ * @author dhs
+ * @date 2022/11/10
+ */
+@SpringBootApplication
+public class RainQBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainQBizApplication.class, args);
+    }
+
+}
---
+++ rainQ-biz/src/test/java/com/dhs/rainqbiz/RainQBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhs.rainqbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainQBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
